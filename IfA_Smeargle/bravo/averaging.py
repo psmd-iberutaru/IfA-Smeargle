Added configuration factory class; fixed a bug in the averaging method.

IfA_Smeargle/bravo/averaging.py
@@ -12,8 +12,8 @@ first X and last X frames, adapting them and outputting them.
 """
 
 
-def average_endpoints(fits_file, start_chunk, end_chunk, frame_exposure_time,
-                      write_file=True, alternate_name=None):
+def average_endpoints_per_second(fits_file, start_chunk, end_chunk, frame_exposure_time,
+                                 write_file=True, alternate_name=None):
     """ This function reads a fits file and computes its end section values.
 
     This function reads in a fits file of 3 dimensions, averaging some 
@@ -46,13 +46,13 @@ def average_endpoints(fits_file, start_chunk, end_chunk, frame_exposure_time,
     """
 
     # Test for the different cases of the fits file.
-    if (isinstance(fits_file,str)):
+    if (isinstance(fits_file, str)):
         hdu_obj, header, data = meta_faa.smeargle_open_fits_file(fits_file)
-    elif (isinstance(fits_file,ap_fits.HDUList)):
+    elif (isinstance(fits_file, ap_fits.HDUList)):
         hdu_obj = fits_file 
         header = fits_file[0].header
         data = fits_file[0].data
-    elif (isinstance(fits_file,ap_fits.PrimaryHDU)):
+    elif (isinstance(fits_file, ap_fits.PrimaryHDU)):
         hdu_obj = ap_fits.HDUList([fits_file])
         header = fits_file.header
         data = fits_file.data
@@ -74,14 +74,14 @@ def average_endpoints(fits_file, start_chunk, end_chunk, frame_exposure_time,
                                            "different sizes, this is unusual but acceptable."))
 
     # Calculate the medians.
-    start_median = np.median(data[-start_chunk:],axis=0)
-    end_median = np.median(data[:end_chunk],axis=0)
+    start_median = np.median(data[:start_chunk],axis=0)
+    end_median = np.median(data[-end_chunk:],axis=0)
 
     # Subtracting and normalizing over the time span, starting and ending
     # at respective midpoints; integer multiplication/division is required  
     # because  of the discrete nature of frames.
     integration_time = (data.shape[0] - (start_chunk//2 + end_chunk//2)) * frame_exposure_time
-    final_data = (start_median - end_median)/integration_time
+    final_data = (end_median-start_median)/integration_time
 
     # Check to see if the user provided an alternate name.
     if ((alternate_name is not None) and (isinstance(alternate_name,str))):
@@ -95,5 +95,51 @@ def average_endpoints(fits_file, start_chunk, end_chunk, frame_exposure_time,
         with warn.catch_warnings():
             warn.simplefilter("ignore", category=OverwriteWarning)
             hdu_file = meta_faa.smeargle_write_fits_file(writing_file_name, header, final_data)
+
+    return hdu_file
+
+
+def average_endpoints_per_kilosecond(fits_file, start_chunk, end_chunk, frame_exposure_time,
+                                     write_file=True, alternate_name=None):
+    """ This function reads a fits file and computes its end section values.
+
+    This function reads in a fits file of 3 dimensions, averaging some 
+    top chunk and bottom chunk of their "temporal" axis, normalizing
+    and dividing over a timespan. The time is measured in kilo-seconds. This
+    is basically a wrapper function around the per second version.
+
+    If there is no temporal axis, this program raises an error.
+    
+    Parameters
+    ----------
+    fits_file : string or Astropy HDUList file
+        This is the fits file that will be modified, or at least have its
+        values calculated from.
+    start_chunk : int
+        The exact number of frames from the beginning that will be median-ed.
+    end_chunk : int
+        The exact number of frames from the bottom that will be median-ed.
+    frame_exposure_time : float
+        The duration, per frame (in seconds), of each exposure.
+    write_file : boolean (optional)
+        If true, the Astropy HDUL object is written to a fits file.
+    alternate_name : string (optional)
+        An alternate fits file name to write the data too instead of the 
+        first provided one.
+
+    Returns
+    -------
+    hdu_file : Astropy HDUList file
+        The HDUList object of the written file.
+    """
+
+    # If frame_exposure_time is in per seconds, this converts it to per 
+    # kilosecond, as desired. 
+    new_frame_exposure_time = frame_exposure_time * 0.001 
+
+    # Feed it through.
+    hdu_file = average_endpoints_per_second(fits_file, start_chunk, end_chunk, 
+                                            new_frame_exposure_time,
+                                            write_file=write_file, alternate_name=alternate_name)
 
     return hdu_file