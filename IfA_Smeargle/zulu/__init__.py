bbe04cc600b65b01e030046ed62cefb430119e5a
Sparrow <psmd.iberutaru@gmail.com>
2019-07-12 12:55:24 -1000

    Created the Zulu line; the overall (rather like a wrapper module) around the IfA Smeargle code.

---
+++ IfA_Smeargle/zulu/__init__.py
@@ -0,0 +1,5 @@
+# Import the main.
+from IfA_Smeargle.zulu.zulu_main import *
+
+# And the lines within each folder
+from IfA_Smeargle.zulu import dectector_pipelines as lines
---
+++ IfA_Smeargle/zulu/zulu_main.py
@@ -0,0 +1,8 @@
+"""
+The ZULU line is the final cumulation of all other lines, in a way, it is
+the script that dictates how the data should be manipulated. Each array
+type has its own functional line, while each subset of these arrays should
+contain their own configuration file (from Yankee). 
+
+Each functional line is contained within their own file.
+"""
