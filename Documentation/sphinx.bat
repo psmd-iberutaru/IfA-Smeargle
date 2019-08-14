0fc8c9ab7434178f42aeaa797210e1365abadca1
Sparrow <psmd.iberutaru@gmail.com>
2019-08-13 14:39:12 -1000

    Still more updates to documentation; mostly stylistic this time.

--- Documentation/sphinx.bat
+++ Documentation/sphinx.bat
@@ -1,12 +1,17 @@
-@echo off
+::@echo off
 
-:: Go out to the mrimary directory for api compliation
+:: It might be helpful to remove the inital build.
+del _build /Q
+
+:: Go out to the primary directory for api compliation
 cd ..
 sphinx-apidoc -e -f -o Documentation/_source/python_docstrings ./IfA_Smeargle
 
 :: Go back into the doc directory and complile the html
 cd Documentation
-make clean && make html && make latex
+call make clean
+call make html
+call make latex
 
 :: Go to the LaTeX and compile
-make latexpdf
+call _build\latex\make.bat
