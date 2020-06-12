2e199a0f9d030875523dfcfd051725dd310a0b37
Sparrow <psmd.iberutaru@gmail.com>
2020-06-11 21:50:14 -1000

    Bug: Fixed issue with the last script not being printed if there are an odd number of scripts.

--- ifa_smeargle/special.py
+++ ifa_smeargle/special.py
@@ -37,7 +37,6 @@ def script_special_create_tutorial_configuration_file(config):
 
     # All done.
     return None
-
 def script_special_list_scripts(config):
     """ This lists all of the scripts in alphabetical order in 
     two columns.
@@ -69,10 +68,22 @@ def script_special_list_scripts(config):
     # Format the key list in the two columns, going across first
     # so the alphabetical list is spread across the two.
     printed_lines = []
-    for evendex, odddex in zip(np.arange(0, n_keys, 2), 
-                               np.arange(1, n_keys, 2)):
-        temp_line = ' '.join([sorted_script_keys[evendex].ljust(max_length),
-                               sorted_script_keys[odddex].ljust(max_length)])
+    for evendex, odddex in zip(np.arange(0, n_keys + 1, 2), 
+                               np.arange(1, n_keys + 1, 2)):
+        # This is the left entry of the line.
+        try:
+            temp_left_entry = sorted_script_keys[evendex].ljust(max_length)
+        except IndexError:
+            # The default empty line if there was none to print.
+            temp_left_entry = '-' * max_length
+        # This is the right entry of the line.
+        try:
+            temp_right_entry = sorted_script_keys[odddex].ljust(max_length)
+        except IndexError:
+            # The default empty line if there was none to print.
+            temp_right_entry = '-' * max_length
+        # Compile the line itself.
+        temp_line = ' '.join([temp_left_entry, temp_right_entry])
         printed_lines.append(temp_line)
 
     # Display the information as normal information. (Some fancy 
@@ -81,7 +92,7 @@ def script_special_list_scripts(config):
                          "{hline} \n"
                          "{script_print}"
                          "\n"
-                         .format(hline=''.join(['-' for __ in range(49)]),
+                         .format(hline=''.join(['=' for __ in range(49)]),
                                  script_print='\n'.join(printed_lines)))
     
     # All done.
