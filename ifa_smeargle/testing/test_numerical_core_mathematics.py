7e2246e43ac64f064e3d6bc1e323ce36e0b76152
Sparrow <psmd.iberutaru@gmail.com>
2020-06-22 17:41:30 -1000

    Feature: Add tests for the mean, median, and std computations.

--- ifa_smeargle/testing/test_numerical_core_mathematics.py
+++ ifa_smeargle/testing/test_numerical_core_mathematics.py
@@ -11,6 +11,7 @@ import sympy as sy
 import math
 
 import ifa_smeargle.core as core
+import ifa_smeargle.masking as mask
 import ifa_smeargle.testing as test
 
 def test_ifas_large_integer_array_product():
@@ -54,4 +55,151 @@ def test_ifas_large_integer_array_product():
                              .format(check=CHECK_B10LOG, log=product_log10))
     assert math.isclose(product_log10, CHECK_B10LOG), b10log_assert_message
     # All done.
+    return None
+
+
+def test_ifas_masked_mean():
+    """ This tests the mean computation with masked arrays."""
+
+    # Creating the testing array of integers.
+    test_array = test.base.create_prime_test_array(shape=(5,5))
+
+    # Creating the mask for this array.
+    column_indexes = [0,1,3,4]
+    row_indexes = [1,4,2,3]
+    mask_array = mask.mask_single_pixels(data_array=test_array,
+                                         column_indexes=column_indexes,
+                                         row_indexes=row_indexes)
+
+    # Creating a masked array.
+    masked_array = np_ma.array(test_array, mask=mask_array)
+
+    # The mean.
+    mean = core.math.ifas_masked_mean(array=masked_array)
+
+    # Test the mean against the expected value.
+    CHECK_STRING = '41.80952380952380952380952'
+    CHECK_NUMBER = sy.Float(CHECK_STRING)
+
+    # Checking the mean itself.
+    assert_message = ("The check mean value is: {check}  "
+                      "The mean value is: {mean} "
+                      "The array is: \n {array}"
+                      .format(check=CHECK_NUMBER, mean=mean,
+                              array=masked_array))
+    assert math.isclose(mean, CHECK_NUMBER), assert_message
+    # All done.
+    return None
+
+def test_ifas_masked_median():
+    """ This tests the median computation with masked arrays."""
+
+    # Creating the testing array of integers.
+    test_array = test.base.create_prime_test_array(shape=(5,5))
+
+    # Creating the mask for this array.
+    column_indexes = [0,1,3,4]
+    row_indexes = [1,4,2,3]
+    mask_array = mask.mask_single_pixels(data_array=test_array,
+                                         column_indexes=column_indexes,
+                                         row_indexes=row_indexes)
+
+    # Creating a masked array.
+    masked_array = np_ma.array(test_array, mask=mask_array)
+
+    # The median.
+    median = core.math.ifas_masked_median(array=masked_array)
+
+    # Test the median against the expected value.
+    CHECK_STRING = '41'
+    CHECK_NUMBER = sy.Integer(CHECK_STRING)
+
+    # Checking the mean itself.
+    assert_message = ("The check median value is: {check}  "
+                      "The median value is: {median} "
+                      "The array is: \n {array}"
+                      .format(check=CHECK_NUMBER, median=median,
+                              array=masked_array))
+    assert median == CHECK_NUMBER, assert_message
+    # All done.
+    return None
+
+def test_ifas_masked_std():
+    """ This tests the standard deviation computation with masked 
+    arrays."""
+
+    # Creating the testing array of integers.
+    test_array = test.base.create_prime_test_array(shape=(5,5))
+
+    # Creating the mask for this array.
+    column_indexes = [0,1,3,4]
+    row_indexes = [1,4,2,3]
+    mask_array = mask.mask_single_pixels(data_array=test_array,
+                                         column_indexes=column_indexes,
+                                         row_indexes=row_indexes)
+
+    # Creating a masked array.
+    masked_array = np_ma.array(test_array, mask=mask_array)
+
+    # The std.
+    std = core.math.ifas_masked_std(array=masked_array)
+
+    # Test the population std against the expected value.
+    CHECK_STRING = '28.11137551075322183368774'
+    CHECK_NUMBER = sy.Float(CHECK_STRING)
+
+    # Checking the mean itself.
+    assert_message = ("The check std value is: {check}  "
+                      "The std value is: {std} "
+                      "The array is: \n {array}"
+                      .format(check=CHECK_NUMBER, std=std,
+                              array=masked_array))
+    assert math.isclose(std, CHECK_NUMBER), assert_message
+    # All done.
+    return None
+
+def test_ifas_robust_mean():
+    """ This tests the computation of means using a more robust 
+    approach."""
+
+    # Creating the testing array of integers.
+    test_array = test.base.create_prime_test_array(shape=(5,5))
+
+    robust_mean = core.math.ifas_robust_mean(array=test_array)
+
+    # Test the mean against the expected value.
+    CHECK_STRING = '42.4'
+    CHECK_NUMBER = sy.Float(CHECK_STRING)
+
+    # Checking the mean itself.
+    assert_message = ("The check mean value is: {check}  "
+                      "The mean value is: {mean} "
+                      "The array is: \n {array}"
+                      .format(check=CHECK_NUMBER, mean=robust_mean,
+                              array=test_array))
+    assert math.isclose(robust_mean, CHECK_NUMBER), assert_message
+    # All done.
+    return None
+
+def test_ifas_robust_std():
+    """ This tests the computation of means using a more robust 
+    approach."""
+
+    # Creating the testing array of integers.
+    test_array = test.base.create_prime_test_array(shape=(5,5))
+
+    robust_std = core.math.ifas_robust_std(array=test_array)
+
+    # Test the mean against the expected value.
+    CHECK_STRING = '28.880443209895517'
+    CHECK_NUMBER = sy.Float(CHECK_STRING)
+
+    # Checking the mean itself.
+    assert_message = ("The check mean value is: {check}  "
+                      "The std value is: {std} "
+                      "The array is: \n {array}"
+                      .format(check=CHECK_NUMBER, std=robust_std,
+                              array=test_array))
+    assert math.isclose(robust_std, CHECK_NUMBER), assert_message
+    # All done
     return None
